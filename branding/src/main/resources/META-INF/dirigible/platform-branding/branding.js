c057605f06eb3c79472cf826d1ab19418a1048a7
delchev <delchevn@gmail.com>
2025-03-02 08:21:26 +0200

    new branding

--- branding/src/main/resources/META-INF/dirigible/platform-branding/branding.js
+++ branding/src/main/resources/META-INF/dirigible/platform-branding/branding.js
@@ -9,6 +9,7 @@
  * SPDX-FileCopyrightText: 2022 codbex or an codbex affiliate company and contributors
  * SPDX-License-Identifier: EPL-2.0
  */
+
 const brandingInfo = {
     name: 'codbex',
     brand: 'codbex',
@@ -20,3 +21,30 @@ const brandingInfo = {
     },
     logo: '/services/web/platform-branding/images/logo.svg',
 };
+
+if (!top.hasOwnProperty('PlatformBranding')) top.PlatformBranding = {
+    name: 'codbex',
+    brand: 'codbex',
+    brandUrl: 'https://www.codbex.com/',
+    icons: {
+        faviconIco: '/services/web/platform-branding/images/favicon.ico',
+    },
+    logo: '/services/web/platform-branding/images/logo.svg',
+    keyPrefix: 'codbex'
+};
+
+function getBrandingInfo() {
+    if (top.hasOwnProperty('PlatformBranding')) return top.PlatformBranding;
+    throw Error("PlatformBranding is not set!");
+}
+
+function setBrandingInfo({ name, brand, brandUrl, icons, logo, keyPrefix } = {}) {
+    if (name) top.PlatformBranding.name = name;
+    if (brand) top.PlatformBranding.brand = brand;
+    if (brandUrl) top.PlatformBranding.brandUrl = brandUrl;
+    if (icons) {
+        if (icons['faviconIco']) top.PlatformBranding.icons.faviconIco = icons['faviconIco'];
+    }
+    if (logo) top.PlatformBranding.logo = logo;
+    if (keyPrefix) top.PlatformBranding.keyPrefix = keyPrefix;
+}
