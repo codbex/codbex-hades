5ed8499e2582bd872617164d2e65d6d7f1d163e7
Iliyan Velichkov <velichkov.iliyan@gmail.com>
2025-03-20 14:56:37 +0200

    Improve app lifecycle logging (#251)

--- application/src/main/java/com/codbex/hades/HadesApplication.java
+++ application/src/main/java/com/codbex/hades/HadesApplication.java
@@ -28,8 +28,15 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 public class HadesApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(HadesApplication.class, args);
+    private static long startedAt;
+
+    public static long getStartedAt() {
+        return startedAt;
     }
 
+    public static void main(String[] args) {
+        startedAt = System.currentTimeMillis();
+        System.out.println("------- Application is starting -------");
+        SpringApplication.run(HadesApplication.class, args);
+    }
 }
