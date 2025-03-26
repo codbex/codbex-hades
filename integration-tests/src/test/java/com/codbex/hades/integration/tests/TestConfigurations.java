667c921757f106b3be3ecc8f4b4ce1035f1ff3f7
Iliyan Velichkov <velichkov.iliyan@gmail.com>
2025-03-26 17:03:56 +0200

    Add more integration tests from dirigible

--- integration-tests/src/test/java/com/codbex/hades/integration/tests/TestConfigurations.java
+++ integration-tests/src/test/java/com/codbex/hades/integration/tests/TestConfigurations.java
@@ -8,7 +8,7 @@
  * SPDX-FileCopyrightText: 2022 codbex or an codbex affiliate company and contributors
  * SPDX-License-Identifier: EPL-2.0
  */
-package com.codbex.hades.ui.tests;
+package com.codbex.hades.integration.tests;
 
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.ComponentScan;
