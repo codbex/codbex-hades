4e3903073dd4e598b8dd5ca675f18b52faf28e19
Iliyan Velichkov <velichkov.iliyan@gmail.com>
2025-03-31 16:28:52 +0300

    Add dirigible tests suite (#259)

---
+++ integration-tests/src/test/java/com/codbex/hades/integration/tests/DirigibileCommonTestSuiteIT.java
@@ -0,0 +1,14 @@
+package com.codbex.hades.integration.tests;
+
+import org.eclipse.dirigible.integration.tests.api.SecurityIT;
+import org.eclipse.dirigible.integration.tests.ui.tests.DatabasePerspectiveIT;
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@SelectClasses({//
+        DatabasePerspectiveIT.class, //
+        SecurityIT.class, //
+})
+public class DirigibileCommonTestSuiteIT {
+}
